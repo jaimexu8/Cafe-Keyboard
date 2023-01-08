0f7f1badfcc8eb4a85aca31adeb8d192b15f5e18
Jaime Xu <xu1545@purdue.edu>
2023-01-07 22:41:22 -0500

    added quotable api, style changes, and html additions

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,28 @@
-const QUOTE_API = "http://api.quotable.io/random"
+const typingText = document.querySelector(".typing-text p"),
+inputField = document.querySelector(".wrapper .input-field"),
+tryAgainButton = document.querySelector(".content button");
 
-function loadParagraph() {
+document.addEventListener("DOMContentLoaded", () => {
+    function loadParagraph() {
+        typingText.innerHTML =
+        "The future is completely open, and we are writing it moment to moment."
+    }
+    function initType() { }
+    function resetGame() { }
 
-}
+    async function updateQuote() {
+        // Fetch a random quote from the Quotable API
+        const response = await fetch("http://api.quotable.io/random");
+        const data = await response.json();
+        if (response.ok) {
+            updateQuote.textContent = data.content;
+        } else {
+            updateQuote.textContent = "An error occurred";
+            console.log(data);
+        }
+    }
 
-function initType() {
-
-}
-
+    loadParagraph();
+    inputField.addEventListener("input", initType);
+    tryAgainButton.addEventListener("click", resetGame);
+});
